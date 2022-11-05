dcdfcdfba0d314bfde699a6be66a6971f8d4d44f
Thomas Wood <woodthom@gmail.com>
2022-11-05 20:20:30 +0000

    Architecture diagram

---
+++ images/generate_diagram.py
@@ -0,0 +1,16 @@
+from diagrams import Cluster, Diagram
+from diagrams.azure.compute import AppServices
+from diagrams.aws.database import ElastiCache, RDS
+from diagrams.aws.network import ELB
+from diagrams.aws.network import Route53
+from diagrams.generic.os import Ubuntu
+from diagrams.generic.device import Tablet
+from diagrams.programming.language import Python, Java
+
+with Diagram("Harmony architecture", show=False):
+
+    browser = Tablet("User browser")
+    dash = Python("Plotly Dash\nfront end\nand business logic\n(Python, GPT-2)")
+    tika = Java("Tika app service\nfor processing PDFs\n(Java)")
+
+    browser >> dash >> tika
