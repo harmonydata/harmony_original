dc74fd85232e98fd07408ca675758315ff366bcc
Thomas Wood <woodthom@gmail.com>
2023-02-07 15:17:08 +0000

    Updated build script

--- front_end/build_deploy.sh
+++ front_end/build_deploy.sh
@@ -1,3 +1,5 @@
+#az login
+#az acr login --name grammartool
 export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g`
 docker build -t harmony --build-arg COMMIT_ID=$COMMIT_ID .
 docker tag harmony regprotocolsfds.azurecr.io/harmony:$COMMIT_ID
