46c5dbe3dfe0cf1126d262bdef6a9ea0dcf7344b
Thomas Wood <woodthom@gmail.com>
2023-04-19 20:51:58 +0100

    update docs

--- images/generate_diagram.py
+++ images/generate_diagram.py
@@ -10,7 +10,7 @@ from diagrams.programming.language import Python, Java
 with Diagram("Harmony architecture", show=False):
 
     browser = Tablet("User browser")
-    dash = Python("Plotly Dash\nfront end\nand business logic\n(Python, GPT-2)")
+    dash = Python("Plotly Dash\nfront end\nand business logic\n(Python, transformer model)")
     tika = Java("Tika app service\nfor processing PDFs\n(Java)")
 
     browser >> dash >> tika
